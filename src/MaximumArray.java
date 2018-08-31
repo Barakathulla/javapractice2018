292b6a3296d303dae4ac40592c67964556344751
Barakathulla <shaikbnisar@gmail.com>
2018-08-31 09:41:39 +0530

    NA

---
+++ src/MaximumArray.java
@@ -0,0 +1,28 @@
+
+public class MaximumArray {
+
+	static int maxDifference(int[] a) {
+
+		int max_diff = a[1] - a[0];
+		int i, j;
+		if (max_diff > 0) {
+			for (i = 0; i < a.length; i++) {
+				for (j = i + 1; j < a.length; j++) {
+					if (a[j] - a[i] > max_diff)
+						max_diff = a[j] - a[i];
+				}
+			}
+			return max_diff;
+		} else
+			return -1;
+	}
+
+	public static void main(String[] args) {
+
+		MaximumArray maxdif = new MaximumArray();
+		int arr[] = { 1, 1, 0, -9, 0 };
+		System.out.println("Maximum differnce is " + maxdif.maxDifference(arr));
+
+	}
+
+}
